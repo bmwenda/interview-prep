ce0a51e4b16d925a7197e610a38a7eeb32a86323
Boniface Mwenda <bonny.mwenda04@gmail.com>
2023-06-27 19:50:01 +0300

    Benchmark factorial methods

--- ruby/factorial.rb
+++ ruby/factorial.rb
@@ -1,16 +1,39 @@
+require 'benchmark'
+
 class InvalidInteger < StandardError; end
 
 # recursion
-def factorial(n)
+def factorial_recursion(n)
   raise InvalidInteger, 'Number must be greater than zero' if n.negative?
   return 1 if n < 2
 
-  n * factorial(n - 1)
+  n * factorial_recursion(n - 1)
 end
 
 # iteration
-def factorial(n)
-  # TODO
+def factorial_iter(n)
+  raise InvalidInteger, 'Number must be greater than zero' if n.negative?
+
+  factorial = 1
+  1.upto(n) do |num|
+    factorial *= num
+  end
+  factorial
 end
 
-# TODO: benchmark
+# Benchmark
+Benchmark.bmbm do |x|
+  x.report('recursion') { factorial_recursion(10_073) }
+  x.report('iteration') { factorial_iter(10_073) }
+end
+
+=begin
+Rehearsal ---------------------------------------------
+recursion   0.049798   0.024016   0.073814 (  0.075264)
+iteration   0.045139   0.011237   0.056376 (  0.057772)
+------------------------------------ total: 0.130190sec
+
+                user     system      total        real
+recursion   0.039090   0.007500   0.046590 (  0.047430)
+iteration   0.038264   0.005881   0.044145 (  0.045397)
+=end
