b71839796b63a3b5e2c161036feacd773c0de059
Boniface Mwenda <bonny.mwenda04@gmail.com>
2023-08-08 21:53:33 +0300

    Add sample input

--- ruby/tic_tac_toe.rb
+++ ruby/tic_tac_toe.rb
@@ -1,5 +1,15 @@
 # Solves for an arbitrary board of size n
 # Checks winner after every move to reduce searches to 4 (row, column, two diagonals)
+# example input:
+# [
+#   ['X', 0, 0],
+#   ['O', 0, 1],
+#   ['X', 1, 1],
+#   ['O', 0, 2],
+#   ['X', 2, 2]
+# ]
+# player X wins => diagonal
+
 class TicTacToe
   attr_reader :board, :moves, :board_size, :winner
 
@@ -77,3 +87,56 @@ class TicTacToe
     valid_cells.include?(position)
   end
 end
+
+
+
+game2 =
+[
+  ['X',0,0],
+  ['O',0,1],
+  ['X',1,1],
+  ['O',0,2],
+]
+# No winner
+
+game3 = [
+  ['X',0,0],
+  ['O',0,1],
+  ['X',0,1]
+]
+# Raise error
+
+game4 = [
+  ['X', 1, 2],
+  ['O', 0, 0],
+  ['X', 1, 1],
+  ['O', 1, 0],
+  ['X', 2, 2],
+  ['O', 2, 0]
+]
+# player O wins => first column
+
+game5 = [
+  ['X', 1, 1],
+  ['O', 0, 1],
+  ['X', 1, 0],
+  ['O', 2, 1],
+  ['X', 1, 2],
+  ['O', 0, 0]
+]
+# player X wins => middle row
+
+game6 = [
+  ['X', 0, 1],
+  ['O', 0, 2],
+  ['X', 1, 0],
+  ['O', 1, 1],
+  ['X', 2, 1],
+  ['O', 2, 0]
+]
+# player O wins => anti diagonal
+
+game = TicTacToe.new(game1, board_size = 3)
+game.play
+p game.board
+p game.winner
