46d6b0ff766e8bb8ab9e71788855365ff4224d60
Boniface Mwenda <bonny.mwenda04@gmail.com>
2023-11-28 20:33:49 +0300

    Update valid parens

--- ruby/valid_parentheses.rb
+++ ruby/valid_parentheses.rb
@@ -1,12 +1,15 @@
 # Given a string with just the characters '(', ')', '{', '}', '[', ']',
 # determine if a given input is valid
 
-# borrowing from acceptable sequences and dyck paths: https://brilliant.org/wiki/catalan-numbers/
-# all sequences of each parens must add to 0
 def valid_parenthesis?(str)
-  paren_refs = { '(' => 1, ')' => -1, '{' => 2, '}' => -2, '[' => 3, ']' => -3 }
-  values = []
-  str.chars.each { |char| values << paren_refs[char] }
-
-  values.sum == 0
+  parens = { ')' => '(', '}' => '{', ']' => '[' }
+  stack = []
+  str.chars.each do |char|
+    if parens.keys.include?(char) && parens[char] == stack[-1]
+      stack.pop
+    else
+      stack.append(char)
+    end
+  end
+  stack.empty?
 end
